4c1340a6eba56bb8982156b91502f19944866cd1
yelizavetaob <74356753+yelizavetaob@users.noreply.github.com>
2020-12-07 21:08:56 +0200

    Update AbstractTaskList.java

--- src/main/java/ua/edu/sumdu/j2se/obolonska/tasks/AbstractTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/obolonska/tasks/AbstractTaskList.java
@@ -1,8 +1,9 @@
 package ua.edu.sumdu.j2se.obolonska.tasks;
 
-import java.util.stream.Stream;
+import java.util.stream.*;
 
-public abstract class AbstractTaskList {
+public abstract class AbstractTaskList implements Iterable<Task>{
+
     public abstract void add(Task task);
 
     public abstract boolean remove(Task task);
@@ -11,7 +12,7 @@ public abstract class AbstractTaskList {
 
     public abstract Task getTask(int index);
 
-    public AbstractTaskList incoming(int from, int to) throws CloneNotSupportedException {
+    public final AbstractTaskList incoming(int from, int to) throws CloneNotSupportedException {
         AbstractTaskList taskList = (AbstractTaskList) this.clone();
         for (int i = 0; i < this.size(); i++) {
             if (this.getTask(i).nextTimeAfter(from) < from && this.getTask(i).nextTimeAfter(from) >= to) {
@@ -21,5 +22,4 @@ public abstract class AbstractTaskList {
         return taskList;
     }
 }
-
 
